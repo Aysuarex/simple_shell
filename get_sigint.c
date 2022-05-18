9c68b8eb5ad14c4a9f4881415b20d8250b4f82a8
Ismail Jamiu Babatunde <32511206+phantware@users.noreply.github.com>
2022-05-18 21:56:08 +0100

    Update get_sigint.c

--- get_sigint.c
+++ get_sigint.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- * get_sigint - Handle the crtl + c call in prompt
- * @sig: Signal handler
+ * get_sigint - Handle the crtl + c call in prompt.
+ * @sig: Signal handler.
  */
 void get_sigint(int sig)
 {
